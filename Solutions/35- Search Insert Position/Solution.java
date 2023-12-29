ac4899577331d51ef793dbf83489913a75c22f5d
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2023-12-29 10:51:38 -0800

    Search Insert Position

---
+++ Solutions/35- Search Insert Position/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+
+        while (left < right) {
+            int middle = (left + right) / 2;
+
+            if (target > nums[middle]) {
+                left = middle + 1;
+            } else if (target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+
+        if (target <= nums[left])
+            return left;
+        else if (target > nums[left])
+            return left + 1;
+
+        return left;
+    }
+
+}
