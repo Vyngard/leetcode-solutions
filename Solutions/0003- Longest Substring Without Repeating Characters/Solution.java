0f86186e122e61e72d410f2a06ffe9e89ebda601
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-10 13:11:38 -0800

    update Longest Substring Without Repeating Characters

--- Solutions/0003- Longest Substring Without Repeating Characters/Solution.java
+++ Solutions/0003- Longest Substring Without Repeating Characters/Solution.java
@@ -1,30 +1,28 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Solution {
+class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> set = new HashSet<>();
+        Map<Character, Integer> map = new HashMap<>();
         int left = 0;
         int right = 0;
-        int maxCount = 0;
-        for (var ch : s.toCharArray()) {
-            if (!set.contains(ch)) {
-                right++;
-                set.add(ch);
-            } else {
-                while (left < right) {
-                    set.remove(s.charAt(left));
-                    left++;
-                    if (!set.contains(ch)) {
-                        right++;
-                        set.add(ch);
-                        break;
-                    }
+        int answer = 0;
+        int size;
 
-                }
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            if (!map.containsKey(ch)) {
+                map.put(ch, right);
+                right++;
+            } else {
+                left = Math.max(map.get(ch) + 1, left); // we make sure that the left pointer doesn't go back if there is a duplicate character
+                map.put(ch, right);
+                right++;
             }
-            maxCount = Math.max(maxCount, right - left);
+            size = (right - left);
+            answer = Math.max(size,answer);
         }
-        return maxCount;
+
+        return answer;
     }
 }
