f3fd138bef6b1a326e0c027d44aeb5b3fab7528d
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-12 10:59:03 -0800

    update Reverse Integer

--- Solutions/0007- Reverse Integer/Solution.java
+++ Solutions/0007- Reverse Integer/Solution.java
@@ -1,22 +1,18 @@
-public class Solution {
+class Solution {
     public int reverse(int x) {
+        boolean negative = x < 0;
         int answer = 0;
-        int z = x;
         x = Math.abs(x);
 
-        while (x != 0) {
+        while (x > 0) {
+           if (answer > (Integer.MAX_VALUE / 10) || -answer < (Integer.MIN_VALUE / 10))
+               return 0;
 
-            if (answer > Integer.MAX_VALUE / 10 || answer < Integer.MIN_VALUE / 10)
-                return 0;
-
-            answer = (answer * 10) + (x % 10);
-            x /= 10;
-
+           answer *= 10;
+           answer = answer + (x % 10);
+           x /= 10;
         }
-        if (z < 0)
-            return -1 * answer;
 
-        return answer;
+        return negative ? (-answer) : answer;
     }
-
 }
