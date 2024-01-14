89b6f46e7006e17a470759da08c073488773e165
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-14 11:45:02 -0800

    update Palindrome Number

--- Solutions/0007- Reverse Integer/Solution.java
+++ Solutions/0007- Reverse Integer/Solution.java
@@ -1,18 +1,13 @@
 class Solution {
-    public int reverse(int x) {
-        boolean negative = x < 0;
-        int answer = 0;
-        x = Math.abs(x);
-
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int reverse = 0;
+        
         while (x > 0) {
-           if (answer > (Integer.MAX_VALUE / 10) || -answer < (Integer.MIN_VALUE / 10))
-               return 0;
-
-           answer *= 10;
-           answer = answer + (x % 10);
-           x /= 10;
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
         }
-
-        return negative ? (-answer) : answer;
+        
+        return temp == reverse;
     }
 }
