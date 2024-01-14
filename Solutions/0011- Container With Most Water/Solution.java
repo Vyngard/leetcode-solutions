26ef4425e11e85393d82ff17231fa2973dafd6fe
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-14 12:13:09 -0800

    update Container With Most Water

--- Solutions/0011- Container With Most Water/Solution.java
+++ Solutions/0011- Container With Most Water/Solution.java
@@ -1,25 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
-        var left = 0;
-        var right = height.length - 1;
+        int left = 0;
+        int right = height.length - 1;
         int area = 0;
-        int temp;
 
         while (left < right) {
-            temp = (right - left) * Math.min(height[left], height[right]);
-
-            if (temp > area)
-                area = temp;
-
-//            area = Math.max(((right - left) * Math.min(height[left], height[right])),area);  // you can use this line instead of upper lines
-
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+          int temp = (right - left) * Math.min(height[left],height[right]);
+          if (temp > area)
+              area = temp;
+          
+          if (height[left] < height[right])
+              left++;
+          else 
+              right--;
         }
-
         return area;
     }
 }
