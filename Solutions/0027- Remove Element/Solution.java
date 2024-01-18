b79536f7a67f5a80aa0e8753b977400b8b495129
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-18 11:34:51 -0800

    update remove element

--- Solutions/0027- Remove Element/Solution.java
+++ Solutions/0027- Remove Element/Solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int k = 0;
-
         for (int num : nums) {
             if (num != val) {
                 nums[k] = num;
