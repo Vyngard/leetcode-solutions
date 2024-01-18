6fe1bb2e9e7086d3586f618f83f5dbf7dae91eaa
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-18 10:39:11 -0800

    update Swap Nodes in Pairs

--- Solutions/0024- Swap Nodes in Pairs/Solution.java
+++ Solutions/0024- Swap Nodes in Pairs/Solution.java
@@ -1,29 +1,34 @@
 class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
     public ListNode swapPairs(ListNode head) {
-        ListNode newList = new ListNode();
-        var pointer = newList;
-        var current = head;
-        var next = head;
+        ListNode dummy = new ListNode();
+        var first = head;
+        var second = head;
+        var current = dummy;
 
-        while (current != null && current.next != null) {
-            next = current.next;
-            var reserve = next.next;
-
-            current.next = null;
-            next.next = current;
-            pointer.next = next;
-            pointer = pointer.next.next;
-
-            current = reserve;
-
+        while (first != null && first.next != null) {
+            second = first.next;
+            var temp = second.next;
+            
+            current.next = second;
+            second.next = first;
+            first.next = null;
+            current = current.next.next;
+            
+            first = temp;
         }
 
-        if (current != null) {
-            pointer.next = current;
-        }
+        if (first != null)
+            current.next = first;
+        
 
-
-
-        return newList.next;
+        return dummy.next;
     }
 }
