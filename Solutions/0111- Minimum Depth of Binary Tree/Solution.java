369f372acf43c598ecf4bcfa1cc99c5722e8afdf
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-21 13:14:25 -0800

    Minimum Depth of Binary Tree

---
+++ Solutions/0111- Minimum Depth of Binary Tree/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null)
+            return 1 + minDepth(root.right);
+
+        if (root.right == null)
+            return 1 + minDepth(root.left);
+
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
