6f8c5a70998716467987a0d19a7a93aba9be00f3
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-23 11:19:51 -0800

    update Search in Rotated Sorted Array

--- Solutions/0033- Search in Rotated Sorted Array/Solution.java
+++ Solutions/0033- Search in Rotated Sorted Array/Solution.java
@@ -4,26 +4,24 @@ class Solution {
         int right = nums.length - 1;
 
         while (left <= right) {
-            int middle = (left + right) / 2;
-
+            int middle = (left + right + 1) / 2;
             if (nums[middle] == target) {
                 return middle;
             }
             if (nums[left] <= nums[middle]) {
-                if (nums[left] <= target && target <= nums[middle]) {
+                if  (nums[left] <= target && target <= nums[middle])
                     right = middle - 1;
-                } else {
+                else
                     left = middle + 1;
-                }
             } else {
-                if (nums[middle] <= target && target <= nums[right]) {
+                if (nums[middle] <= target && target <= nums[right])
                     left = middle + 1;
-                } else {
+                else
                     right = middle - 1;
-                }
             }
         }
 
         return -1;
     }
+
 }
