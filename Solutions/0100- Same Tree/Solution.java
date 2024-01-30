7d04ce2ed91ca94ccd2bc8329ac8d3c288c40bf6
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-01-30 10:27:55 -0800

    update Same Tree

--- Solutions/0100- Same Tree/Solution.java
+++ Solutions/0100- Same Tree/Solution.java
@@ -1,25 +1,15 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
         if (p == null && q == null)
             return true;
-        else if (p == null || q == null)
+
+        if (p == null && q != null || p != null && q == null || p.val != q.val)
             return false;
-        else if (p.val != q.val)
-            return false;
 
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
 
-//        var left = isSameTree(p.left, q.left);
-//        if (!left)
-//            return false;
-//
-//        var right = isSameTree(p.right, q.right);
-//        if (!right)
-//            return false;
-//
-//        return true;
-
-        // you can write the line instead of previous lines
-        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+        return left && right;
     }
+
 }
