5c99e01dae0177d3463624426ce97802c11326ed
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-02-23 10:30:11 -0800

    update delete cuplicate emails

--- Solutions/0196- Delete Duplicate Emails/Solution2.sql
+++ Solutions/0196- Delete Duplicate Emails/Solution2.sql
@@ -1,6 +1,3 @@
-DELETE p2
-FROM
-    person p1
-    JOIN person p2 ON p1.email = p2.email
-WHERE
-    p1.id < p2.id;
+DELETE FROM person p1
+USING person p2
+WHERE p1.email = p2.email AND p1.id > p2.id;
