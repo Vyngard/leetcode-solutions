5ed1c3b7e22044ff8b8b832c6ade75c6a83dab4d
Pirooz Ekhtiyari <pirooz1371@gmail.com>
2024-07-22 10:28:42 -0700

    update Longest common prefix

--- Solutions/0014- Longest Common Prefix/Solution.java
+++ Solutions/0014- Longest Common Prefix/Solution.java
@@ -1,17 +1,22 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder answer = new StringBuilder();
+        if (strs.length == 0)
+            return "";
+        String first = strs[0];
+        StringBuffer answer = new StringBuffer();
 
-        for (int i = 0; i < strs[0].length(); i++) {
-            char ch = strs[0].charAt(i);
+        for (int i = 0; i < first.length(); i++) {
+            char ch = first.charAt(i);
             for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[j].charAt(i) != ch)
+                String temp = strs[j];
+                if (i >= temp.length() || first.charAt(i) != temp.charAt(i)) {
                     return answer.toString();
+                }
             }
-            answer.append(ch);
+            answer.append(first.charAt(i));
         }
 
         return answer.toString();
-    }
 
+    }
 }
